a084fb863516c4b291e8c8c4fb682ccdca0f729d
Iancusio <57413363+Iancusio@users.noreply.github.com>
2019-12-12 18:35:07 +0100

    Update practica19_IanMartinez.sh

--- practica19_IanMartinez.sh
+++ practica19_IanMartinez.sh
@@ -5,11 +5,11 @@ LOG="${1}"
 
 if [[ ! -e "${LOG}" ]]
 then 
-  echo "No sha pogut obrir el fitxer: ${LOG_FILE}" >&2
+  echo "No sha pogut obrir el fitxer: ${LOG}" >&2
   exit 1
 fi
 
-echo 'Count,IP,Location'
+echo 'Count,IP,Ubicacion'
 
 grep Failed ${LOG} | awk '{print $(NF - 3)}' | sort | uniq -c | sort -nr |  while read COUNT IP
 do
